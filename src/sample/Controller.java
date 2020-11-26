7588ea36e25bd58c101d38ae7c111bdd2f583faa
Celeste <cecuercu@gmail.com>
2020-11-26 16:17:38 +0100

    Corregido

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
 public class Controller {
@@ -18,12 +19,30 @@ public class Controller {
     @FXML
     private Button idButton;
 
+    @FXML
     public void showPassword(MouseEvent event) {
         idShowPassword.setText(idTFPassword.getText());
     }
 
+    @FXML
+    public void onClickedMouseShowPassword(MouseEvent event) {
+        idShowPassword.setText("Mostrar contraseña");
+    }
+
+    @FXML
     public void pressButton(MouseEvent event) {
         idButton.setText("Botón pulsado");
+
+    }
+
+    @FXML
+    public void soltarButton(MouseEvent event) {
+        idButton.setText("Guardar");
+    }
+
+    @FXML
+    public void onKeyPressed(KeyEvent key) {
+        idShowPassword.setText(key.getCharacter());
     }
 
 }
